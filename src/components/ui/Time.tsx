541d426a54a1cd173330bbfb4e6aba209d08fad0
mdubelbeis <mason.dubelbeis@gmail.com>
2022-10-08 11:17:24 -0500

    responsiveness text

--- src/components/ui/Time.tsx
+++ src/components/ui/Time.tsx
@@ -10,7 +10,7 @@ const Time: React.FC = () => {
   });
 
   return (
-    <p className="font-digital text-3xl py-10 lg:text-9xl text-center w-10/12 lg:w-full lg:h-96 bg-black text-green-500 rounded-xl lg:py-32 drop-shadow-xl">
+    <p className="font-digital text-4xl py-10 lg:text-9xl text-center w-10/12 lg:w-full lg:h-96 bg-black text-green-500 rounded-xl lg:py-32 drop-shadow-xl">
       {output}
     </p>
   );
