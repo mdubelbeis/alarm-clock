e865d87a6fd2b7c468e0babe0e8799251092b360
mdubelbeis <mason.dubelbeis@gmail.com>
2022-10-10 11:17:22 -0500

    fixed fetch req

--- src/components/Clock/Weather.tsx
+++ src/components/Clock/Weather.tsx
@@ -1,35 +1,41 @@
 import { useState, useEffect } from "react";
 
 const Weather: React.FC = () => {
-  const [weatherData, setWeatherData] = useState<{}[]>([]);
+  const [weatherData, setWeatherData] = useState<[]>([]);
+  //? HOW TO GET THIS TO WORK WITH GEOLOCATION API()?
   const [location, setLocation] = useState<string>("Austin");
 
   useEffect(() => {
     getWeatherData();
-  }, []);
+  }, [weatherData]);
 
   const getWeatherData = async () => {
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "2c7bf43f9emsh4e853e3c9087f81p1f2733jsnd1452e643658",
+        "X-RapidAPI-Key": import.meta.env.VITE_REACT_API_KEY,
         "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
       },
     };
+
     try {
       const response = await fetch(
         "https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=38.5&lat=-78.5",
         options
       );
       const data = await response.json();
-      setWeatherData(data);
-      console.log(weatherData);
+      setWeatherData(data.data);
     } catch (error) {
       // getWeatherData(); //? Keep Trying to fetch if continues to fail?
       console.log("ERROR: " + error);
     }
   };
-  return <div>Weather Module</div>;
+
+  return (
+    <div>
+      <small>{weatherData}</small>
+    </div>
+  );
 };
 
 export default Weather;
