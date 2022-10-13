9cfc55daae1414f27998635d718b8debfebd57ba
mdubelbeis <mason.dubelbeis@gmail.com>
2022-10-12 19:14:46 -0500

    added padding to timing

--- src/components/ui/Time.tsx
+++ src/components/ui/Time.tsx
@@ -14,8 +14,8 @@ const Time: React.FC = () => {
   });
 
   return (
-    <div className="relative w-full px-4 py-10 md:py-20 lg:py-28">
-      <p className="flex items-center justify-center drop-shadow-xl shadow-green-500 font-digital text-5xl sm:text-6xl md:text-7xl px-4 lg:py-10 lg:text-8xl text-center w-full ">
+    <div className="relative w-full px-4 py-10 md:py-20 lg:pt-40">
+      <p className="flex items-center justify-center drop-shadow-xl shadow-green-500 font-digital text-5xl sm:text-6xl md:text-7xl px-4 lg:py-10 lg:text-9xl text-center w-full">
         <span className="shadow-lg opacity-60">{output}</span>
       </p>
     </div>
