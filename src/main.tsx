0bc8cbd2bf79cd529ca8c6aca8fdb3c723bb7a36
mdubelbeis <mason.dubelbeis@gmail.com>
2022-10-20 08:24:03 -0500

    setup folder structure for store

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
 import "./index.css";
 
 import App from "./App";
@@ -12,17 +15,19 @@ import Notes from "./pages/Notes";
 import Calculator from "./pages/Calculator";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/clock" element={<Clock />} />
-        <Route path="clock/stop-watch" element={<StopWatch />} />
-        <Route path="clock/timer" element={<Timer />} />
-        <Route path="/todo" element={<Todo />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/calculator" element={<Calculator />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/clock" element={<Clock />} />
+          <Route path="clock/stop-watch" element={<StopWatch />} />
+          <Route path="clock/timer" element={<Timer />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/calculator" element={<Calculator />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
