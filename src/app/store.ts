0249c6d818a2dcc4b16787172a24099ad51d5151
mdubelbeis <mason.dubelbeis@gmail.com>
2022-11-10 10:53:16 -0600

    removed weatherReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducers from "../app/Todo/TodoSlice";
 import clockReducers from "../app/Clock/ClockSlice";
-import weatherReducers from "../app/Clock/WeatherSlice";
 
 export const store = configureStore({
   reducer: {
     todoStore: todoReducers,
     clockStore: clockReducers,
-    weatherStore: weatherReducers,
   },
 });
 
